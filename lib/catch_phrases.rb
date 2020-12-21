ff4163fe635423408ac5a22521ad66e1b2a35ede
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-21 03:27:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,15 @@
 
-
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status
   phrase = "It's-a me, Mario!"
   puts phrase
 end
+
+
+def toadstool
+  puts status
+end
+
